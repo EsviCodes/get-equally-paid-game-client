54bb5cc5f48ffda5d5e8ad569dd2a6f80a1ec60c
EsviCodes <esther.js.vis@gmail.com>
2019-11-20 17:14:57 +0100

    Machine Learning works

--- src/sketches/test-sketch.js
+++ src/sketches/test-sketch.js
@@ -10,21 +10,24 @@ export default function testSketch(p) {
   let video;
 
   let classifier;
+  let label = "waiting";
+
+  console.log("label", label);
 
   // STEP 1: Load the model! STEP 2: Start classifying
   p.preload = () => {
-    classifier = ml5
-      .imageClassifier(
-        "https://teachablemachine.withgoogle.com/models/RtE5ECt6/model.json"
-      )
-      .then(classifier => {
-        console.log("classifier", classifier);
-        return classifier.classify(video);
-      })
-      .then(results => {
-        console.log("results", results);
-      })
-      .catch(error => console.error(error));
+    classifier = ml5.imageClassifier(
+      "https://teachablemachine.withgoogle.com/models/RtE5ECt6/model.json"
+    );
+    //   .then(classifier => {
+    //     console.log("classifier", classifier);
+    //     return classifier.classify(video);
+    //   })
+    //   .then(results => {
+    //     // the first object is always the one with the highest confidence score
+    //     console.log("results", results[0].label);
+    //   })
+    //   .catch(error => console.error(error));
   };
 
   p.setup = () => {
@@ -34,16 +37,25 @@ export default function testSketch(p) {
     video.hide();
 
     // STEP 2: Start classifying
-    //classifyVideo();
+    classifyVideo();
   };
 
   // STEP 2 classify!
-  //   const classifyVideo = () => {
-  //     // const classify = ml5.classify();
-  //     // console.log("classify", classify);
-  //     // classifier is a promise
-  //     //classifier.classify(video, gotResults);
-  //   };
+  const classifyVideo = () => {
+    classifier
+      .then(classifier => {
+        console.log("classifier", classifier);
+        return classifier.classify(video);
+      })
+      .then(results => {
+        // the first object is always the one with the highest confidence score
+        //console.log("results", results[0].label);
+        label = results[0].label;
+        console.log("label", label);
+        classifyVideo();
+      })
+      .catch(error => console.error(error));
+  };
 
   p.draw = () => {
     p.background(0);
@@ -52,14 +64,18 @@ export default function testSketch(p) {
     p.image(video, 0, 0);
 
     // STEP 4: Draw the label
+    p.textSize(32);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.fill(255);
+    p.text(label, p.width / 2, p.height - 16);
   };
 
   // STEP 3: Get the classification!
-  const gotResults = (error, results) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log(results);
-  };
+  //   const gotResults = (error, results) => {
+  //     if (error) {
+  //       console.error(error);
+  //       return;
+  //     }
+  //     console.log(results);
+  //   };
 }
