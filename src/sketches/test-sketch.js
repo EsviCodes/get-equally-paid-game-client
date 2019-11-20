3b22a75c59d15738c5449c74d76a21ebb22c81f1
EsviCodes <esther.js.vis@gmail.com>
2019-11-20 17:00:12 +0100

    Get results from ml5

--- src/sketches/test-sketch.js
+++ src/sketches/test-sketch.js
@@ -1,11 +1,31 @@
+import * as ml5 from "ml5";
+
 export default function testSketch(p) {
   // Add some header info
   // For TM template code
 
+  console.log("ml5", ml5);
+
   // Video
   let video;
 
-  // STEP 1: Load the model!
+  let classifier;
+
+  // STEP 1: Load the model! STEP 2: Start classifying
+  p.preload = () => {
+    classifier = ml5
+      .imageClassifier(
+        "https://teachablemachine.withgoogle.com/models/RtE5ECt6/model.json"
+      )
+      .then(classifier => {
+        console.log("classifier", classifier);
+        return classifier.classify(video);
+      })
+      .then(results => {
+        console.log("results", results);
+      })
+      .catch(error => console.error(error));
+  };
 
   p.setup = () => {
     p.createCanvas(640, 520);
@@ -14,9 +34,16 @@ export default function testSketch(p) {
     video.hide();
 
     // STEP 2: Start classifying
+    //classifyVideo();
   };
 
   // STEP 2 classify!
+  //   const classifyVideo = () => {
+  //     // const classify = ml5.classify();
+  //     // console.log("classify", classify);
+  //     // classifier is a promise
+  //     //classifier.classify(video, gotResults);
+  //   };
 
   p.draw = () => {
     p.background(0);
@@ -28,4 +55,11 @@ export default function testSketch(p) {
   };
 
   // STEP 3: Get the classification!
+  const gotResults = (error, results) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(results);
+  };
 }
