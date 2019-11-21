f99d79291d43f6ff47e8c670454b37e842a8baa3
EsviCodes <esther.js.vis@gmail.com>
2019-11-21 09:20:16 +0100

    Added info to home component

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Game based on the classic Snake Game to promote awareness of the gender pay gap 
 - [ml5 docs](https://ml5js.org/)
 - [Promises and Callback support in ml5](https://learn.ml5js.org/docs/#/tutorials/promises-and-callbacks)
 - [The Coding Train - Teachable Machine 1: Image Classification](https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html)
+- [Information about the Dutch gender pay gap (Dutch)](https://www.womeninc.nl/thema/loonkloof/)
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,22 @@ export default class Home extends Component {
   render() {
     return (
       <div>
+        <p className="facts">
+          The difference between the hourly wage of men and women in the
+          Netherlands is on average 15%.
+          <br></br>
+          <br></br>
+          This is €300.000,- in a working life that women earn less than men.
+          <br></br>
+          <br></br>In the Dutch tech field it's worse with a 18,6% difference.
+          <br></br>
+          <br></br>
+          November 6 2019 was Equal Pay Day. This means that women work for free
+          the rest of the year.
+          <br></br>
+          <br></br>
+          So...
+        </p>
         <Link className="link" to="/get-equally-paid">
           <button>Let's play a game</button>
         </Link>
