e9aa41eab04b843a835b44514c3f7c5ac7572a78
EsviCodes <esther.js.vis@gmail.com>
2019-11-21 11:49:45 +0100

    Final styling

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ class App extends Component {
         <Route path="/get-equally-paid" exact component={Game} />
         <Route path="/closing-the-gap" exact component={Info} />
         <Route path="/money-value" exact component={Money} />
-        <footer>Made by Esvi Codes</footer>
+        <footer>
+          <a className="footerLink" href="https://github.com/EsviCodes">
+            Made by Esvi Codes
+          </a>
+        </footer>
       </div>
     );
   }
